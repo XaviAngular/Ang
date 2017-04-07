fd5f6052eba3e633ca340618e51f08bd982a1f0e
XaviAngular <xavcet@gmail.com>
2017-04-07 16:50:58 +0200

    Form de contacto

--- app/contacto/contacto.component.ts
+++ app/contacto/contacto.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {takeAwayService} from "../takeaway.service";
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.css'],
+  providers:[takeAwayService]
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _service:takeAwayService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form:any){
+  	this._service.addContact(form).subscribe(
+          result => {
+            if (result.status=="success"){
+             console.log("Contacto recibido");
+            }
+            else {
+              alert("Error petición Mysql");
+            }
+          },
+          error =>{
+            alert('Error al enviar contacto');
+          }
+        )
   }
 
 }
